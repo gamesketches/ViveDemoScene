2a7f070fc831f9396eeaf4720275b72d7390397d
gamesketches <sam.von.ehren@gmail.com>
2016-06-09 15:01:30 -0400

    Trying out attaching some objects to shit

---
+++ Assets/movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	CharacterController characterController;
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vert = Input.GetAxis("Vertical");
+
+		characterController.Move(new Vector3(0f, vert, 0f));
+	}
+}
